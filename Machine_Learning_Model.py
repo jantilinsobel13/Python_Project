5d719d41dca53412c77ad5b7d801f77a5ed2f67d
jantilinsobel13 <jantilinsobel@gmail.com>
2026-06-06 21:12:10 +0530

    Update Machine_Learning_Model.py

--- Machine_Learning_Model.py
+++ Machine_Learning_Model.py
@@ -10,8 +10,7 @@ from sklearn.feature_extraction.text import CountVectorizer\
 from sklearn.model_selection import train_test_split\
 from sklearn.naive_bayes import MultinomialNB\
 from sklearn.metrics import accuracy_score, confusion_matrix\
-\
-\
+
 data = \{\
     "text": [\
         "Win a free iPhone now", \
@@ -21,24 +20,20 @@ data = \{\
     ],\
     "label": ["phishing", "safe", "phishing", "safe"]\
 \}\
-\
+
 df = pd.DataFrame(data)\
-\
-\
+
 vectorizer = CountVectorizer()\
 X = vectorizer.fit_transform(df["text"])\
 y = df["label"]\
-\
-\
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)\
-\
-\
+
 model = MultinomialNB()\
 model.fit(X_train, y_train)\
-\
-\
+
 y_pred = model.predict(X_test)\
-\
+
 print("Accuracy:", accuracy_score(y_test, y_pred))\
 print("Confusion Matrix:\\n", confusion_matrix(y_test, y_pred))\
 }
