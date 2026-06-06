a030018ddce90e4dbf098ddb3f418e1288357e46
jantilinsobel13 <jantilinsobel@gmail.com>
2026-06-06 21:18:04 +0530

    Update Web Scraping.py

--- Web Scraping.py
+++ Web Scraping.py
@@ -7,14 +7,12 @@
 
 \f0\fs24 \cf0 import requests\
 from bs4 import BeautifulSoup\
-\
+
 url = "https://veloriatech.com/"\
 response = requests.get(url)\
-\
-\
+
 soup = BeautifulSoup(response.text, "html.parser")\
-\
-\
+
 print("Veloria Tech Links:")\
 for link in soup.find_all("a", href=True):\
     text = link.get_text(strip=True)\
